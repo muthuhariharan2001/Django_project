d76d3309769b118c44172b8aa0095a390d509ee4
Muthu Hariharan <164756456+muthuhariharan2001@users.noreply.github.com>
2025-02-19 20:00:47 +0530

    Update settings.py

--- email_sender/settings.py
+++ email_sender/settings.py
@@ -51,13 +51,13 @@ MIDDLEWARE = [
 ]
 
 
-DEFAULT_FROM_EMAIL = 'muthuhariharan2001@gmail.com'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'muthuhariharan2001@gmail.com'
-EMAIL_HOST_PASSWORD = 'uulr wujl qdix uvsv'
+# DEFAULT_FROM_EMAIL = 'muthuhariharan2001@gmail.com'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'muthuhariharan2001@gmail.com'
+# EMAIL_HOST_PASSWORD = 'uulr wujl qdix uvsv'
 
 ROOT_URLCONF = 'email_sender.urls'
 
